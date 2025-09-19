1b567ebcfe544711db472faf414c27cd29168d9b
Nathan <natethepcspecialist@gmail.com>
2025-09-18 22:03:33 -0400

    Remove unused src/modules/entities directory
    - Deleted legacy barrel export file that was not being used anywhere
    - All entity imports are done directly from their source locations
    - This improves code clarity and removes dead code

--- src/modules/entities/index.ts
+++
@@ -1,9 +0,0 @@
-// Re-export entities from their new modular locations
-export * from '../../dj/dj.entity';
-export * from '../../entities/user.entity';
-export * from '../../favorite/favorite.entity';
-export * from '../../parser/parsed-schedule.entity';
-export { ParseStatus } from '../../parser/parsed-schedule.entity';
-export * from '../../show/show.entity';
-export { DayOfWeek } from '../../show/show.entity';
-export * from '../../vendor/vendor.entity';
